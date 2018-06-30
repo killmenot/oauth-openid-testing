aed273d9169b794560ad68e511ade6dc92660125
Alexey Kucherenko <alexei.kucherenko@gmail.com>
2018-07-01 00:12:51 +0300

    update keys

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const jwt = require('jwt-simple');
 const uuidv4 = require('uuid/v4');
 
 const app = express();
-const jwtSecret = 'foobar';
+const baseUrl = 'https://oauth-openid-testing.herokuapp.com';
+const strip = (str) => str.split('\n').filter(x => x && !x.startsWith('-----')).join('')
 
 app.get('/healthcheck', (req, res) =>
   res.json({
@@ -14,9 +15,25 @@ app.get('/healthcheck', (req, res) =>
   })
 );
 
-app.get('/favicon.ico', (req, res) => res.status(204));
+app.get('/favicon.ico', (req, res) => {
+  res.status(204)
+});
 
-app.get('/cicauth/realms/NHS/protocol/openid-connect/auth', (req, res) => res.redirect(`${req.query.redirect_uri}?code=mzgybGPAr4tGi9fd`));
+app.get('/cicauth/realms/NHS', (req, res) => {
+  res.json({
+    'realm': 'NHS',
+    'public_key': strip(process.env.PUBLIC_KEY || ''),
+    'token-service': `${baseUrl}/cicauth/realms/NHS/protocol/openid-connect`,
+    'account-service': `${baseUrl}/cicauth/realms/NHS/account`,
+    'admin-api': `${baseUrl}/cicauth/admin`,
+    'tokens-not-before': 0
+    }
+  })
+});
+
+app.get('/cicauth/realms/NHS/protocol/openid-connect/auth', (req, res) => {
+  res.redirect(`${req.query.redirect_uri}?code=mzgybGPAr4tGi9fd`)
+});
 
 app.post('/cicauth/realms/NHS/protocol/openid-connect/token', (req, res) => {
   const data = {
@@ -29,7 +46,7 @@ app.post('/cicauth/realms/NHS/protocol/openid-connect/token', (req, res) => {
 
   res.json({
     session_state: uuidv4(),
-    id_token: jwt.encode(data, jwtSecret, 'RS256')
+    id_token: jwt.encode(data, process.env.PRIVATE_KEY, 'RS256')
   });
 });
 
